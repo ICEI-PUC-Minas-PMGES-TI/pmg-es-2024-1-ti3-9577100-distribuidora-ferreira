0b082d7e5f5e8f2a352dcd0df33ce05ca94729c8
gramos22 <gabrielrmzf@gmail.com>
2024-03-15 09:57:01 -0300

    chore: fix duplicated produtos folder

--- Codigo/frontend/src/app/modules/produtos/produtos.module.ts
+++ Codigo/frontend/src/app/modules/produtos/produtos.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProdutosComponent } from './produtos/produtos.component';
+import { ProdutosComponent } from './produtos.component';
 import { MaterialModule } from 'src/app/material-module';
 
 
