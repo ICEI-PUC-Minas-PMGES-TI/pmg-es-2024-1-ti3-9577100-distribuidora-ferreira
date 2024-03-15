90ad845c7214187d4068a774a1b4ffb2b02832bb
gramos22 <gabrielrmzf@gmail.com>
2024-03-14 23:57:40 -0300

    feat: create produtos component

--- Codigo/frontend/src/app/modules/produtos/produtos.module.ts
+++ Codigo/frontend/src/app/modules/produtos/produtos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProdutosComponent
+  ],
   imports: [
     CommonModule
   ]
