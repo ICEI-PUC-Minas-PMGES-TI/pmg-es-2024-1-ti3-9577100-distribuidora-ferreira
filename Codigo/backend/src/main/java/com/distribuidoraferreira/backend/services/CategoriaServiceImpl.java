3739ed47fec217431e2cae1a8b6b3a16042a5ff5
douglas <douglasgmachado98@gmail.com>
2024-03-19 14:22:41 -0300

    chore: remove dependencies that's not used

--- Codigo/backend/src/main/java/com/distribuidoraferreira/backend/services/CategoriaServiceImpl.java
+++ Codigo/backend/src/main/java/com/distribuidoraferreira/backend/services/CategoriaServiceImpl.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.distribuidoraferreira.backend.dtos.CategoriaRequest;
 import com.distribuidoraferreira.backend.dtos.CategoriaResponse;
-import com.distribuidoraferreira.backend.dtos.ProdutoResponse;
 import com.distribuidoraferreira.backend.mappers.CategoriaMapper;
-import com.distribuidoraferreira.backend.mappers.ProdutoMapper;
 import com.distribuidoraferreira.backend.models.Categoria;
 import com.distribuidoraferreira.backend.repositories.CategoriaRepository;
 
@@ -53,5 +51,6 @@ public class CategoriaServiceImpl implements CategoriaService {
 
         return categoriasResponse;
     }
-    // TODO Criar exceptions e fazer response messages melhores
+
+
 }
